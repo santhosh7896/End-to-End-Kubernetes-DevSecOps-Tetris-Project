38a5801340b9acd7511c9e5b6ca15c3a063f0bf7
root <root@ip-172-31-7-211.ap-south-1.compute.internal>
2025-05-26 07:37:15 +0000

    Fix iam-role.tf: convert JSON to Terraform HCL IAM policy document

--- EKS-TF/iam-role.tf
+++ EKS-TF/iam-role.tf
@@ -1,21 +1,32 @@
-{
-  "Effect": "Allow",
-  "Action": [
-    "s3:PutObject",
-    "s3:GetObject",
-    "s3:ListBucket"
-  ],
-  "Resource": [
-    "arn:aws:s3:::my-eks-devsecops-santhosh-2025",
-    "arn:aws:s3:::my-eks-devsecops-santhosh-2025/*"
-  ]
-},
-{
-  "Effect": "Allow",
-  "Action": [
-    "dynamodb:GetItem",
-    "dynamodb:PutItem",
-    "dynamodb:DeleteItem"
-  ],
-  "Resource": "arn:aws:dynamodb:ap-south-1:*:table/terraform-lock"
+data "aws_iam_policy_document" "s3_dynamodb_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::my-eks-devsecops-santhosh-2025",
+      "arn:aws:s3:::my-eks-devsecops-santhosh-2025/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = [
+      "arn:aws:dynamodb:ap-south-1:*:table/terraform-lock"
+    ]
+  }
 }
+
+resource "aws_iam_policy" "eks_backend_policy" {
+  name   = "eks-backend-policy"
+  policy = data.aws_iam_policy_document.s3_dynamodb_access.json
+}
+
